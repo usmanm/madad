4973b499c68984faefceceab6073ffc8eedf0c3f
faaez <faaez.ulhaq@gmail.com>
2013-09-14 16:33:22 -0400

    adding index.html

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin; admin.autodiscover()
 
 urlpatterns = patterns('',
+	url('', 'website.views.index'),
+	url(r'^/', 'website.views.index'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     # Examples:
