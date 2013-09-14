0d656fe03f4693184da7229aa1e504c32327e248
faaez <faaez.ulhaq@gmail.com>
2013-09-14 18:13:26 -0400

    tracking files

--- website/urls.py
+++ website/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^social/', include('social.urls')),
+    url(r'^$', 'website.views.index'),
 )
