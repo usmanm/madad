1185e8de889e51badf4e4c082cd46427de76ee18
faaez <faaez.ulhaq@gmail.com>
2013-09-15 11:55:51 -0400

    modifying campaign page

--- campaign/views.py
+++ campaign/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from models import Campaign, Need
+from campaign.models import Campaign
+from campaign.models import Need
+import  datetime
+
 def campaign(request, campaign_id):
   campaign = Campaign.objects.get(hash_id=campaign_id)
   needs = Need.objects.filter(campaign=campaign).all()
@@ -9,11 +12,15 @@ def campaign(request, campaign_id):
   for need in Need.objects.filter(campaign=campaign).all():
     target = need.remaining + need.fulfilled
     needs_status[need] = (need.fulfilled, target , need.fulfilled*100/target)
+
+  time_percentage_elapsed = ((datetime.date.today() - campaign.start_date).total_seconds() * 100)/(campaign.end_date - campaign.start_date).total_seconds()
+  
   return render_to_response(
     'campaign/viewcampaign.html', 
     RequestContext(request, {'campaign':campaign,
                              'needs': needs,
-                             'needs_status':needs_status}))
+                             'needs_status':needs_status,
+                             'time_elapsed':time_percentage_elapsed}))
 
 @login_required
 def create_campaign(request, user_id):
